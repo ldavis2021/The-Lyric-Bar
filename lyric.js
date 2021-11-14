3719ac6736510c7ffca77e0acbd447379da1e5b7
mslatonya <latonya.davis08@gmail.com>
2021-11-13 23:52:47 -0800

    OMDB Homework Complete

--- lyric.js
+++ lyric.js
@@ -1,6 +1,26 @@
 const btn = document.getElementById("btn");
-
+const titleInput = document.getElementById("Title")
+const artistInput = document.getElementById("Artist")
+const lyricdiv = document.getElementById("lyric-div")
 btn.addEventListener('click', function () {
-  var name = document.getElementById("myName").value;
-  alert("Name: " + name);
+
+  let title = titleInput.value.toLowerCase()
+  let artist = artistInput.value.toLowerCase()
+  searchlyrics(artist, title);
+
 });
+
+
+
+async function searchlyrics(artist, title) {
+  let res = await axios.get(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+  console.log(res.data);
+  lyricdiv.innerHTML = res.data.lyrics
+
+}
+
+// const searchWindow;
+// function openWindow()
+// search
+ // var name = document.getElementById("myName").value;
+  // alert("Name: " + name);
